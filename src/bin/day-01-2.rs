15085ca58f832062927f7ea6559c6146abc3d60a
Ivan Smirnov <i.s.smirnov@gmail.com>
2019-12-01 19:15:36 +0000

    Add solution for day-01-2

---
+++ src/bin/day-01-2.rs
@@ -0,0 +1,15 @@
+use std::iter;
+
+use aoc19::parse_ints;
+
+fn compute_fuel(mass: i64) -> i64 {
+    iter::successors(Some(mass), |&f| if f > 6 { Some(f / 3 - 2) } else { None })
+        .skip(1)
+        .sum()
+}
+
+fn main() {
+    let input = include_str!("../../inputs/day-01-1.txt");
+    let answer: i64 = parse_ints(input).map(compute_fuel).sum();
+    println!("{}", answer);
+}
