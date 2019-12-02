c934005b45d4787a61b07bb4d9a1579c34325073
Ivan Smirnov <i.s.smirnov@gmail.com>
2019-12-02 21:32:28 +0000

    Merge day-01-1 and day-01-2 into one file as well

--- src/bin/day-01-1.rs
+++
@@ -1,7 +0,0 @@
-use aoc19::parse_ints;
-
-fn main() {
-    let input = include_str!("inputs/day-01.txt");
-    let answer: i64 = parse_ints::<i64>(input, '\n').map(|x| (x / 3) - 2).sum();
-    println!("{}", answer);
-}
