6381736d04951496e98cf6c6fa7f42335db8a5bb
Ivan Smirnov <i.s.smirnov@gmail.com>
2019-12-05 00:45:15 +0000

    Add solution for day-04-2

--- src/bin/day-04.rs
+++ src/bin/day-04.rs
@@ -2,7 +2,7 @@ fn to_digits(num: &str) -> Vec<i32> {
     num.chars().map(|c| c.to_digit(10).unwrap() as _).collect()
 }
 
-fn is_valid(password: i32) -> bool {
+fn is_valid_1(password: i32) -> bool {
     let digits = to_digits(&password.to_string());
     let (n_same, n_decreasing) =
         (1..digits.len())
@@ -14,11 +14,39 @@ fn is_valid(password: i32) -> bool {
     n_same > 0 && n_decreasing == 0
 }
 
+fn is_valid_2(password: i32) -> bool {
+    let digits = to_digits(&password.to_string());
+    let (n_same, n_decreasing) = (1..digits.len())
+        .map(|i| {
+            (
+                digits[i] - digits[i - 1],
+                if i >= 2 {
+                    Some(digits[i - 1] - digits[i - 2])
+                } else {
+                    None
+                },
+                if i < digits.len() - 1 {
+                    Some(digits[i + 1] - digits[i])
+                } else {
+                    None
+                },
+            )
+        })
+        .fold((0, 0), |acc, d| {
+            let (d, left, right) = d;
+            let is_same = d == 0 && left != Some(0) && right != Some(0);
+            let (n_same, n_decreasing) = acc;
+            (n_same + (is_same as i32), n_decreasing + ((d < 0) as i32))
+        });
+    n_same > 0 && n_decreasing == 0
+}
+
 fn main() {
     let input: (i32, i32) = (152085, 670283);
-    let answer1 = (input.0..=input.1)
-        .map(is_valid)
-        .map(|x| x as i32)
-        .sum::<i32>();
+
+    let answer1: u32 = (input.0..=input.1).map(|x| is_valid_1(x) as u32).sum();
     println!("{}", answer1);
+
+    let answer2: u32 = (input.0..=input.1).map(|x| is_valid_2(x) as u32).sum();
+    println!("{}", answer2);
 }
