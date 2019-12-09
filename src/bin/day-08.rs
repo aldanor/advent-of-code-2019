016ccaa3a2b5835fcbe26491e7061345c7fb1679
Ivan Smirnov <i.s.smirnov@gmail.com>
2019-12-09 22:07:23 +0000

    Add solution for day-08-2

--- src/bin/day-08.rs
+++ src/bin/day-08.rs
@@ -1,3 +1,5 @@
+use std::fmt::{self, Debug};
+
 fn count(slice: &[i64], digit: i64) -> usize {
     slice.iter().filter(|&x| *x == digit).count()
 }
@@ -25,6 +27,37 @@ impl Image {
     pub fn iter_layers(&self) -> impl Iterator<Item = &[i64]> {
         self.data.chunks(self.width * self.height)
     }
+
+    pub fn render(&self) -> Self {
+        let layer_size = self.width * self.height;
+        let n_layers = self.data.len() / layer_size;
+        let rendered = (0..layer_size)
+            .map(|pixel_id| {
+                (0..n_layers)
+                    .map(|layer_id| self.data[layer_id * layer_size + pixel_id])
+                    .skip_while(|&x| x == 2)
+                    .next()
+                    .unwrap_or(0)
+            })
+            .collect();
+        Self {
+            data: rendered,
+            width: self.width,
+            height: self.height,
+        }
+    }
+}
+
+impl Debug for Image {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.data.chunks(self.width).for_each(|row| {
+            row.iter().for_each(|&x| {
+                write!(f, "{}", if x == 1 { "█" } else { " " }).unwrap();
+            });
+            write!(f, "\n").unwrap();
+        });
+        Ok(())
+    }
 }
 
 fn main() {
@@ -34,4 +67,7 @@ fn main() {
     let objective = |s| (count(s, 0), count(s, 1) * count(s, 2));
     let answer1 = image.iter_layers().map(objective).min().unwrap().1;
     println!("{}", answer1);
+
+    let answer2 = image.render();
+    println!("{:?}", answer2);
 }
